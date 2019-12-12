121a79c3debfa0e2614d0ae869a546772997d057
jonathangacos08 <58801315+jonathangacos08@users.noreply.github.com>
2019-12-12 13:54:45 +0800

    Add files via upload
    thankyou ser

---
+++ LabExer1A.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class LabExer1A{
+	public static void main (String[]args){
+		
+		
+		int faveNumber = 69;
+		String faveCartChar = "Zinetsu";
+		char mi = 'M';
+		char[] nicknameArray = {'J', 'o', 'j', 'o',};
+		
+		System.out.println(faveNumber + " is my favorite number.");
+		System.out.println("I love " + faveCartChar + "!");
+		System.out.println("My name is Jonathan " + mi + " Gacos.");
+		System.out.print("You can call me ");
+		System.out.print(nicknameArray[0]);
+		System.out.print(nicknameArray[1]);
+		System.out.print(nicknameArray[2]);
+		System.out.print(nicknameArray[3] + ".");
+		
+
+
+		
+
+		
+		
+	}
+}
